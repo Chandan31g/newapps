2ac8f9e6f8591a4bf828202145db7448abe88940
Chandan31g <55895665+Chandan31g@users.noreply.github.com>
2023-09-28 16:15:16 +0530

    code commit for deployment
    code commit for deployment

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class NavBar extends Component {
-  static propTypes = {
-
-  }
-
+  
   render() {
     return (
       <nav className="navbar navbar-expand-lg bg-dark">
